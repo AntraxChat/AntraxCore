c9c6a2a3ef6382977c85c5652e5075e7c2840408
UpsurgeCerberus <79743969+WhohOfficial@users.noreply.github.com>
2021-03-07 22:47:54 +0100

    Add files via upload

---
+++ Client/Network.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntraxClient.Network
+{
+    class Network
+    {
+        static string BaseAddress { get; set; }
+        static TcpListener _listener { get; set; }
+        static HttpClient _httpClient = new HttpClient();
+
+        static bool tcpServerRunning = false;
+
+        public static class Tcp
+        {
+            public static async Task Initialize(Json.Holder.NetworkProperties holder)
+            {
+                BaseAddress = await File.ReadAllTextAsync(Resource.dNetProp);
+                _httpClient = new HttpClient();
+            }
+            public class Message
+            {
+                public static async Task Broadcast()
+                {
+
+                }
+            }
+        }
+
+        public static class Http
+        {
+            public static async Task<string> Post(string id, string message)
+            {
+                var values = new Dictionary<string, string> {
+                    { "identifier", id },
+                    { "message", message }
+                };
+
+                var response = await _httpClient.PostAsync(BaseAddress, new FormUrlEncodedContent(values));
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
---
+++ Client/Utils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace AntraxClient.Utils
+{
+    class Util
+    {
+        public static class Print
+        {
+            public static void Logo()
+            {
+                var n = Environment.NewLine;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    "████████████████████████████████████████████████████████████████" + n +
+                    " ███████     ██   ████  █        █      ███     ██  ███  ██████ " + n +
+                    "  █████  ███  █    ███  ████  ████  ███  █  ███  ██  █  ██████  " + n +
+                    "   ████       █  █  ██  ████  ████      ██       ███   ██████   " + n +
+                    "  █████  ███  █  ██  █  ████  ████  ███  █  ███  ██  █  ██████   " + n +
+                    " ██████  ███  █  ███    ████  ████  ███  █  ███  █  ███  ██████ " + n +
+                    "████████████████████████████████████████████████████████████████"
+                    );
+            }
+
+            public static void WaterMark()
+            {
+                var bar = "█████████████████████████████";
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(bar);
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Write("Antrax");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(bar + "\n");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static string CurrentUnixTime()
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+
+        public static void Debug(string content, int severity = 0)
+        {
+            string prefix = null;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("[");
+            if (severity == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                prefix = "SUCCESS";
+            }
+            else if (severity == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                prefix = "INFO";
+            }
+            else if (severity == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                prefix = "WARNING";
+            }
+            else if (severity == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                prefix = "ERROR";
+            }
+            else if (severity == 4)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.Black;
+                prefix = "CRITICAL";
+            }
+            else prefix = "no prefix";
+            Console.Write(prefix);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write($"] {content}\n");
+        }
+    }
+}
